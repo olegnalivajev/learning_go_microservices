ec201ceb10802185f337639c695bdc77321035bf
onaliv <olegnalivajev@gmail.com>
2020-04-26 15:22:23 +0100

    edit items package

--- bookstore_users-api/main.go
+++ bookstore_users-api/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/olegnalivajev/learning_go_microservices/bookstore_items-api"
+	"github.com/olegnalivajev/learning_go_microservices/bookstore_items-api/items"
 )
 
 func main() {
